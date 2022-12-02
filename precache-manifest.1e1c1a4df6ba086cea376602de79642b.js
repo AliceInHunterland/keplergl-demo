8dbc00f1e237f624888758280711e0b01b9cad48
AliceInHunterland <inthehunterland@gmail.com>
2022-12-02 15:42:54 +0300

    Updates

---
+++ precache-manifest.1e1c1a4df6ba086cea376602de79642b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c622c9dadb68b0373a3b5aef6649a2e",
+    "url": "/keplergl-demo/index.html"
+  },
+  {
+    "revision": "76ef2af59c3337e21723",
+    "url": "/keplergl-demo/static/css/main.7f862af1.chunk.css"
+  },
+  {
+    "revision": "2b9a62b71c07fefeeec2",
+    "url": "/keplergl-demo/static/js/2.725ce674.chunk.js"
+  },
+  {
+    "revision": "e99bfb2c9ce08567e0bfa16aeaec79f8",
+    "url": "/keplergl-demo/static/js/2.725ce674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ef2af59c3337e21723",
+    "url": "/keplergl-demo/static/js/main.2f3e477d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/keplergl-demo/static/js/main.2f3e477d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316c37cce093fd768378",
+    "url": "/keplergl-demo/static/js/runtime-main.aff1563c.js"
+  }
+]);
